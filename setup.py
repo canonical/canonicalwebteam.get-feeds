b41733e8de24f162b1d72dc2104da8e01ad0dba0
Robin Winslow <robin@robinwinslow.co.uk>
2017-08-03 12:00:08 +0100

    v0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='canonicalwebteam.get-feeds',
-    version='0.1.0',
+    version='0.1.1',
     author='Canonical Webteam',
     url='https://github.com/canonical-webteam/get-feeds',
     packages=[
